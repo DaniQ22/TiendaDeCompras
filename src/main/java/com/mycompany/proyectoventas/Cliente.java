2eafade4156786393cd03453927a1fd0e06f1d33
Daniel Quiñonez <daniel.jose1999@outlook.com>
2023-09-15 15:01:07 -0500

    Proyecto ventas fase 1

---
+++ src/main/java/com/mycompany/proyectoventas/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoventas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    int identificacion;
+    String Nombre;
+    String Apellido;
+    
+    public Cliente(){
+        identificacion = 0;
+        Nombre = "";
+        Apellido = "";
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/proyectoventas/ProyectoVentas.java
@@ -0,0 +1,18 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoventas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
